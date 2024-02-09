f9962f5eb6a059aa59d92d1f876a22c1123d5985
mohit <mv386631@gmail.com>
2024-02-09 18:39:40 +0530

    update db structure

--- database.sql
+++ database.sql
@@ -33,3 +33,42 @@ CREATE TABLE mentees (
     occupation VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+
+-- Quizze Section
+
+CREATE TABLE Quizzes (
+    quiz_id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    creator_id INT REFERENCES Users(user_id),
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- other relevant columns
+);
+
+CREATE TABLE Questions (
+    question_id SERIAL PRIMARY KEY,
+    quiz_id INT REFERENCES Quizzes(quiz_id),
+    question_text TEXT NOT NULL,
+    question_type VARCHAR(20) NOT NULL,
+    -- other relevant columns
+);
+
+CREATE TABLE Options (
+    option_id SERIAL PRIMARY KEY,
+    question_id INT REFERENCES Questions(question_id),
+    option_text TEXT NOT NULL,
+    is_correct BOOLEAN NOT NULL,
+    -- other relevant columns
+);
+
+CREATE TABLE Answers (
+    answer_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(user_id),
+    question_id INT REFERENCES Questions(question_id),
+    selected_option_id INT REFERENCES Options(option_id),
+    answer_text TEXT, -- for fill-in-the-blank questions
+    is_correct BOOLEAN NOT NULL,
+    submission_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- other relevant columns
+);
