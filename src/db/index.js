3296d8bf455b0452346a00124fb26d8bec2c50a7
mohit <mv386631@gmail.com>
2024-03-13 12:42:56 +0530

    test 1

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,3 @@
-// const { Pool } = require("pg");
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "mudita",
-//   password: "123",
-//   port: 5432,
-// });
-
-// module.exports = {
-//   query: (text, params) => pool.query(text, params),
-// };
-
 const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
